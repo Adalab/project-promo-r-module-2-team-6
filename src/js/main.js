9484fb360cbe6775876af4ed7a5506be32f2a230
Elena <elenavargasm9@gmail.com>
2022-10-24 20:01:39 +0200

    add share mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
 
+// NOS SERVIRÁS MÁS ADELANTE PARA ENLAZAR EL BOTON DE TWITTER CON LA URL CREADA
+// const link = document.querySelector('.twitter-share-button');
+// link.href = 'https://twitter.com/intent/tweet?url=';
+
