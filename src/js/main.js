912fca3eedd124bddf48fc0f89caa38fe5de4563
Elena <elenavargasm9@gmail.com>
2022-11-02 13:34:58 +0100

    add partials js

--- src/js/main.js
+++ src/js/main.js
@@ -8,9 +8,11 @@
 //primera flechita por defecto abierta
 
 //variables
+//partes cuestionario
 const design = document.querySelector('.js-design');
 const fill =  document.querySelector('.js-fill');
 const btnCreate =  document.querySelector('.js-btn-create');
+//flechas
 const arrowDesignUp = document.querySelector('.js-arrow-design-up');
 const arrowFillUp = document.querySelector('.js-arrow-fill-up');
 const arrowShareUp = document.querySelector('.js-arrow-share-up');
@@ -25,18 +27,21 @@ function handleClick(event) {
     design.classList.toggle('collapsed');
     fill.classList.add('collapsed');
     btnCreate.classList.add('collapsed');
+    //flechas
     arrowDesignUp.classList.toggle('collapsed');
     arrowDesignDown.classList.toggle('collapsed');
   }else if (event.target.classList.contains('js-arrow-fill-up') || event.target.classList.contains('js-arrow-fill-down')) {
     fill.classList.toggle('collapsed');
     design.classList.add('collapsed');
     btnCreate.classList.add('collapsed');
+    //flechas
     arrowFillUp.classList.toggle('collapsed');
     arrowFillDown.classList.toggle('collapsed');
   }else if (event.target.classList.contains('js-arrow-share-up') || event.target.classList.contains('js-arrow-share-down')) {
     btnCreate.classList.toggle('collapsed');
     design.classList.add('collapsed');
     fill.classList.add('collapsed');
+    //flechas
     arrowShareUp.classList.toggle('collapsed');
     arrowShareDown.classList.toggle('collapsed');
   }
@@ -46,6 +51,7 @@ function handleClick(event) {
 arrowDesignUp.addEventListener('click', handleClick);
 arrowFillUp.addEventListener('click', handleClick);
 arrowShareUp.addEventListener('click', handleClick);
+
 arrowDesignDown.addEventListener('click', handleClick);
 arrowFillDown.addEventListener('click', handleClick);
 arrowShareDown.addEventListener('click', handleClick);
