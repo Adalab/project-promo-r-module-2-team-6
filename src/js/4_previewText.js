d0caa002180c791761f4509d51b59ea8f4c73c20
Natalia-GP <nataliag86@yahoo.es>
2022-11-08 14:15:00 +0100

    update js form

--- src/js/4_previewText.js
+++ src/js/4_previewText.js
@@ -10,17 +10,73 @@
     "github": "mariagar",
     "photo": "data:image/png;base64,2342ba..."
   } */
-let name = '';
-nameInput.addEventListener('input',()=> {
-  
-  name= nameInput.value;
-  console.log(name);
 
-  if(name===''){
-    previewName.innerHTML='Nombre Apellido';
-  }else{
-    
-    previewName.innerHTML=name;
-    console.log(name);
+  const data = {
+    palette: '',
+    name:'',
+    job: '',
+    email: '',
+    linkedin: '',
+    github:'',
+  };
+
+// const nameInput = document.querySelector ('.js-name');
+// const jobInput = document.querySelector ('.js-job');
+// const emailInput = document.querySelector ('.js-email');
+// const phoneInput = document.querySelector ('.js-phone');
+// const linkedinInput = document.querySelector ('.js-linkedin');
+// const githubInput = document.querySelector ('.js-github');
+
+const fillForm= document.querySelector('.fill');
+
+const previewName = document.querySelector ('.js-preview-name');
+const previewJob = document.querySelector ('.js-preview-job');
+
+
+
+function handleInput (event) {
+  event.preventDefault();
+  const selectedInput = event.target.name;
+  console.log(selectedInput);
+  if (selectedInput === 'name'){
+    data.name = event.target.value;
   }
-});
+}
+
+fillForm.addEventListener('input', handleInput);
+
+
+
+
+
+
+
+// nameInput.addEventListener('input',()=> {
+  
+//   data.name= nameInput.value;
+//   console.log(names);
+
+//   if(names===''){
+//     previewName.innerHTML='Nombre Apellido';
+//   }else{
+    
+//     previewName.innerHTML=names;
+//     console.log(names);
+//   }
+// });
+
+// let names = '';
+// jobInput.addEventListener('input',()=> {
+  
+//   names= jobInput.value;
+//   console.log(names);
+
+//   if(names===''){
+//     previewName.innerHTML='Nombre Apellido';
+//   }else{
+    
+//     previewName.innerHTML=names;
+//     console.log(names);
+//   }
+// });
+
